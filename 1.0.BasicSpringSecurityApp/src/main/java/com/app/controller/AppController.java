967f4e666544c62b651827305bf3231a582b88ff
Satya <satyaganesh987@gmail.com>
2021-10-01 11:49:25 +0530

    Initial Commit

---
+++ 1.0.BasicSpringSecurityApp/src/main/java/com/app/controller/AppController.java
@@ -0,0 +1,13 @@
+package com.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppController {
+
+	@GetMapping("/")
+	public String helloSecurity() {
+		return "<h1>Hello Security</h1>";
+	}
+}
