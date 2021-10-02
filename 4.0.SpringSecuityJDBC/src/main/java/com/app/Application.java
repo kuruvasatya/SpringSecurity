807ae10f7cb023761faea606cd774d50960d9e32
Satya <satyaganesh987@gmail.com>
2021-10-02 09:56:45 +0530

    Initial commit: using h2 db, JDBC with Default Schema

---
+++ 4.0.SpringSecuityJDBC/src/main/java/com/app/Application.java
@@ -0,0 +1,17 @@
+package com.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
+
+}
